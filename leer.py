c27d5f1e32650c21ba209dcd24702141df708e9a
Antonio Villanueva Segura <karpvs@yahoo.fr>
2019-09-03 14:18:40 +0200

    Add files via upload
    Una version en python3 del codigo en c anterior

---
+++ leer.py
@@ -0,0 +1,56 @@
+# coding=utf-8
+#Antonio Villanueva Segura 
+#Un ensayo en python3 del programa en c anterior 
+import math
+
+#*******************************************************************************
+def caudal(valor):
+	#Calculo caudal fake funcion
+	return str (valor*2)+' litros/min'
+#*******************************************************************************	
+def presion(valor):
+	#Calculo presion fake funcion
+	return str(round (valor*3,3))+' psi'
+#*******************************************************************************	
+def temperatura(valor):
+	#Calculo temperatura fake funcion
+	return str(valor*4)+' celsius'
+#*******************************************************************************	
+def altura(valor):
+	#Calculo altura fake funcion
+	return str (valor*5)+' m'			
+#*******************************************************************************	
+def obtieneNumero(linea):
+	#Recupera numero float despues del caracter ':' en la linea
+	if ':' in linea:#Verifica que incluye el caracter :
+		return float ( linea.split(':')[1].rstrip())
+#*******************************************************************************		
+def obtieneNombre(linea,variables):
+	#Recupera el nombre en mayusculas en la linea CAUDAL,PRESION ...
+	for nombre in variables:#Recorre las lineas
+		if nombre in linea:#Si existe este nombre e
+			return nombre
+#*******************************************************************************					
+def funcionVariable(nombre,valor):
+	#Imprime el nombre y  resultado para cada variable
+	if nombre=='CAUDAL':
+		print(nombre,' : ',caudal(valor))
+	if nombre=='PRESION':
+		print(nombre,' : ',presion(valor))
+	if nombre=='TEMPERATURA':
+		print(nombre,' : ',temperatura(valor))
+	if nombre=='ALTURA':
+		print(nombre,' : ',altura(valor))						
+	
+
+#*******************************************************************************
+#                               MAIN   LOOP
+#*******************************************************************************
+format(math.pi, '.2f')   # give 2 digits after the point
+variables=('CAUDAL','PRESION','TEMPERATURA','ALTURA')
+
+with open('fichero.txt', 'r') as fichero_texto:
+	for linea in fichero_texto:#Lee linea a linea el fichero
+		funcionVariable(obtieneNombre(linea,variables),obtieneNumero(linea))
+
+
